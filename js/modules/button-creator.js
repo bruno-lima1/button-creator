4af8ec0a2ab14c32570cb5f93eb33928cfa07638
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-27 22:42:44 -0300

    updating project

---
+++ js/modules/button-creator.js
@@ -0,0 +1,60 @@
+export default function buttonCreator() {
+  const form = document.querySelector("[data-form]");
+  const button = document.querySelector("[data-button]");
+  const cssText = document.querySelector("[data-cssText]");
+  if (form && button && cssText) {
+    form.addEventListener("change", handleStyle);
+    function handleStyle(event) {
+      const { name, value } = event.target;
+      changeStyle[name](value);
+      showCssText();
+      saveValues(name, value);
+    }
+    const changeStyle = {
+      element: button,
+      width(value) {
+        this.element.style.width = value + "px";
+      },
+      height(value) {
+        this.element.style.height = value + "px";
+      },
+      borderRadius(value) {
+        this.element.style.borderRadius = value + "px";
+      },
+      fontFamily(value) {
+        this.element.style.fontFamily = value;
+      },
+      backgroundColor(value) {
+        this.element.style.backgroundColor = value;
+      },
+      color(value) {
+        this.element.style.color = value;
+      },
+      texto(value) {
+        this.element.innerHTML = value;
+      },
+      fontSize(value) {
+        this.element.style.fontSize = value + "px";
+      },
+      border(value) {
+        this.element.style.border = value;
+      },
+    };
+    function showCssText() {
+      cssText.innerHTML =
+        "<span>" + button.style.cssText.split("; ").join("; </span><span>");
+    }
+    function saveValues(name, value) {
+      localStorage[name] = value;
+    }
+    function setValues() {
+      const properties = Object.keys(localStorage);
+      properties.forEach((propertie) => {
+        changeStyle[propertie](localStorage[propertie]);
+        form.elements[propertie].value = localStorage[propertie];
+      });
+      showCssText();
+    }
+    setValues();
+  }
+} 
